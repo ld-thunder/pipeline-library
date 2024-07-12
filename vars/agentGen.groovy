27748d50294b0b06da689de6514482f86f2cc9ea
Logan Donley <ldonley@cloudbees.com>
2024-07-12 16:48:02 -0400

    another attempt

--- vars/agentGen.groovy
+++ vars/agentGen.groovy
@@ -1,8 +1,8 @@
 def call(String podTemplatePath, String cloud, String container, Closure body) {
-    def label = UUID.randomUUID().toString()
+    def label = "custom-${UUID.randomUUID().toString()}"
     def podYaml = libraryResource podTemplatePath
 
-    podTemplate(label: label, yaml: podYaml, cloud: cloud)
+    podTemplate(name: 'custom', label: label, yaml: podYaml, cloud: cloud)
     {
         node(label) {
             container(container) {
